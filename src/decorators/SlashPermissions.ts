ed561b5c4b6bc90ff6b16cb7caae58a816180016
bitomic <35199700+bitomic@users.noreply.github.com>
2021-11-12 14:59:57 -0700

    feat(decorators): slash permissions

---
+++ src/decorators/SlashPermissions.ts
@@ -0,0 +1,32 @@
+import { createClassDecorator, createProxy } from '@sapphire/decorators'
+import type { ApplicationCommandPermissionData } from 'discord.js'
+import type { Ctor } from '@sapphire/utilities'
+import type { SlashCommand } from '../framework'
+
+interface ISlashAllowOptions {
+	ids: string[] | string
+	permission: boolean
+	type: 'ROLE' | 'USER'
+}
+
+export function SlashPermissions( { ids, permission, type }: ISlashAllowOptions ): ClassDecorator {
+	ids = Array.isArray( ids ) ? ids : [ ids ]
+	const permissions: ApplicationCommandPermissionData[] = ids.map( id => ( {
+		id,
+		permission,
+		type
+	} ) )
+
+	return createClassDecorator( ( target: Ctor<ConstructorParameters<typeof SlashCommand>, SlashCommand> ) =>
+		createProxy( target, {
+			construct: ( ctor, [
+				context, baseOptions = {}
+			] ) =>
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+				new ctor( context, {
+					...baseOptions,
+					permissions
+				} )
+		} )
+	)
+}
