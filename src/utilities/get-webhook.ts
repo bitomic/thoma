c20cbdb94894f17dfb3126899d94005455f6cd60
bitomic <35199700+bitomic@users.noreply.github.com>
2022-01-01 21:12:14 -0700

    fix(utilities): allow news channels

--- src/utilities/get-webhook.ts
+++ src/utilities/get-webhook.ts
@@ -1,7 +1,7 @@
-import type { TextChannel, Webhook } from 'discord.js'
+import type { NewsChannel, TextChannel, Webhook } from 'discord.js'
 import { container } from '@sapphire/framework'
 
-export const getWebhook = async ( channel: TextChannel ): Promise<Webhook> => {
+export const getWebhook = async ( channel: NewsChannel | TextChannel ): Promise<Webhook> => {
 	const webhooks = await channel.fetchWebhooks()
 	return webhooks.find( w => w.owner?.id === container.client.user?.id ) ?? channel.createWebhook( container.client.user?.username ?? 'null' )
 }
