88a25d6cb38597a8b4e6905e704fbd7ad9d85e29
bitomic <35199700+bitomic@users.noreply.github.com>
2022-01-01 20:22:15 -0700

    feat(utilities): get webhook

---
+++ src/utilities/get-webhook.ts
@@ -0,0 +1,7 @@
+import type { TextChannel, Webhook } from 'discord.js'
+import { container } from '@sapphire/framework'
+
+export const getWebhook = async ( channel: TextChannel ): Promise<Webhook> => {
+	const webhooks = await channel.fetchWebhooks()
+	return webhooks.find( w => w.owner?.id === container.client.user?.id ) ?? await channel.createWebhook( container.client.user?.username ?? 'null' )
+}
