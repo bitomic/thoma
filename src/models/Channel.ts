cc7d97479fff287f5b949fd8406f2ffe3e36e0d7
bitomic <35199700+bitomic@users.noreply.github.com>
2022-03-09 18:38:27 -0600

    feat: channel model

---
+++ src/models/Channel.ts
@@ -0,0 +1,61 @@
+import type { ModelStatic, Model as SequelizeModel } from 'sequelize'
+import type { PieceContext, PieceOptions } from '@sapphire/pieces'
+import type { ChannelTypes } from '../utilities'
+import { DataTypes } from 'sequelize'
+import { Model } from '../framework'
+
+interface IChannel {
+	channel: string
+	guild: string
+	type: ChannelTypes
+}
+
+interface IChannelInterface extends SequelizeModel<IChannel, IChannel>, IChannel {
+}
+
+export class ChannelModel extends Model<IChannelInterface> {
+	public readonly model: ModelStatic<IChannelInterface>
+
+	public constructor( context: PieceContext, options: PieceOptions ) {
+		super( context, {
+			...options,
+			name: 'channels'
+		} )
+
+		this.model = this.container.sequelize.define<IChannelInterface>(
+			'KeyV',
+			{
+				channel: DataTypes.STRING,
+				guild: {
+					primaryKey: true,
+					type: DataTypes.STRING
+				},
+				type: {
+					primaryKey: true,
+					type: DataTypes.STRING
+				}
+			},
+			{
+				tableName: 'Channels',
+				timestamps: false
+			}
+		)
+	}
+
+	public async set( { channel, guild, type }: { channel: string, guild: string, type: ChannelTypes } ): Promise<void> {
+		await this.model.upsert(
+			{ channel, guild, type },
+		)
+	}
+
+	public async get( guild: string, type: ChannelTypes ): Promise<string | null> {
+		const result = await this.model.findOne( { where: { guild, type } } )
+		return result?.getDataValue( 'channel' ) ?? null
+	}
+}
+
+declare global {
+	interface ModelRegistryEntries {
+		channels: ChannelModel
+	}
+}
