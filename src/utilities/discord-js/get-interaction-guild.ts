82a86012517a725978d266a0ef388800fb971ddb
bitomic <35199700+bitomic@users.noreply.github.com>
2022-03-10 22:45:38 -0600

    fix: remove unnecessary eslint disable

--- src/utilities/discord-js/get-interaction-guild.ts
+++ src/utilities/discord-js/get-interaction-guild.ts
@@ -1,6 +1,5 @@
 import type { Guild, Interaction } from 'discord.js'
 import { container } from '@sapphire/framework'
 
-// eslint-disable-next-line require-await
 export const getInteractionGuild = ( interaction: Interaction<'present'> ): Guild | Promise<Guild> => interaction.guild
 		?? container.client.guilds.fetch( interaction.guildId )
