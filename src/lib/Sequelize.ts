6878bc2b35f5feb857fdfdbbaa25524a14ac0a63
bitomic <35199700+bitomic@users.noreply.github.com>
2022-03-13 18:27:56 -0600

    chore(debug): i don't know where is the sqlite path going

--- src/lib/Sequelize.ts
+++ src/lib/Sequelize.ts
@@ -1,6 +1,6 @@
 import { env } from './environment'
 import { Sequelize } from 'sequelize'
-
+console.log( env.SQLITE_PATH )
 export const sequelize = new Sequelize( {
 	dialect: 'sqlite',
 	logging: false,
