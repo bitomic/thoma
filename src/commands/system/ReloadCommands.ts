ebc86e1dfa6b5588f7e2f5eb52ee26f9dc321a32
bitomic <35199700+bitomic@users.noreply.github.com>
2022-07-09 18:04:07 -0600

    fix: improve command to reload commands

--- src/commands/system/ReloadCommands.ts
+++ src/commands/system/ReloadCommands.ts
@@ -5,9 +5,9 @@ import type { CommandOptions } from '@sapphire/framework'
 import { env } from '../../lib'
 
 @ApplyOptions<CommandOptions>( {
-	description: 'Remove all application commands (manually restart after).',
+	description: 'Reload all application commands.',
 	enabled: true,
-	name: 'remove-commands'
+	name: 'reload-commands'
 } )
 export class UserCommand extends Command {
 	public override async registerApplicationCommands( registry: ApplicationCommandRegistry ): Promise<void> {
@@ -24,17 +24,23 @@ export class UserCommand extends Command {
 	}
 
 	public override async chatInputRun( interaction: CommandInteraction ): Promise<void> {
-		void interaction.reply( 'remove-commands' )
+		const t1 = Date.now()
+		await interaction.reply( ':hourglass_flowing_sand: Removing commands. This may take a while...' )
 		await this.container.stores.get( 'models' ).get( 'commands' )
 			.truncate()
 
 		await this.container.client.application?.commands.set( [] )
 		const guilds = await this.container.client.guilds.fetch()
+		await interaction.editReply( `:ballot_box_with_check: Global commands removed.\n:hourglass_flowing_sand: Removing guild-specific commands (checking for ${ guilds.size } guilds).` )
 		for ( const [ _, guild ] of guilds ) {
 			const fetchGuild = await guild.fetch()
 			const guildCommands = await fetchGuild.commands.fetch()
 			if ( guildCommands.size === 0 ) continue
 			await fetchGuild.commands.set( [] )
 		}
+		await interaction.editReply( `:ballot_box_with_check: Global commands removed.\n:ballot_box_with_check: Removing guild-specific commands (checking for ${ guilds.size } guilds).\n:hourglass_flowing_sand: Reloading commands.` )
+		await this.container.stores.get( 'commands' ).loadAll()
+		const t2 = Date.now()
+		await interaction.editReply( `:ballot_box_with_check: Global commands removed.\n:ballot_box_with_check: Removing guild-specific commands (checking for ${ guilds.size } guilds).\n:ballot_box_with_check: Reloading commands.\n:ballot_box_with_check: Done! ${ ( t2 - t1 ) / 1000 } seconds.` )
 	}
 }
