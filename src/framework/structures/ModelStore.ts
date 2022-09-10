0efe3886c232c34fc87996dd3da069853f3c2894
bitomic <35199700+bitomic@users.noreply.github.com>
2022-09-10 12:24:25 -0600

    chore(deps): bump sapphire dependencies

--- src/framework/structures/ModelStore.ts
+++ src/framework/structures/ModelStore.ts
@@ -4,7 +4,6 @@ import type { Model as SequelizeModel } from 'sequelize'
 
 export class ModelStore extends AliasStore<Model<SequelizeModel>> {
 	public constructor() {
-		// @ts-expect-error - Either expect-error or cast to any: https://github.com/sapphiredev/framework/blob/db6febd56afeaeff1f23afce2a269beecb316804/src/lib/structures/CommandStore.ts#L10
 		super( Model, {
 			name: 'models'
 		} )
